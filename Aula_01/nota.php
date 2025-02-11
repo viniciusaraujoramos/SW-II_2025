f4ef3e7e2effa6c77c4cb4e72d776cd74cf20066
Vinicius Araujo Ramos <124802745+viniciusaraujoramos@users.noreply.github.com>
2025-02-11 11:07:07 -0300

    Add files via upload

---
+++ Aula_01/nota.php
@@ -0,0 +1,14 @@
+<?php
+    $NOTA1=7;
+    $NOTA1=8;
+    $NOTA1=9;
+
+    $MEDIA=($NOTA1=$NOTA2=$NOTA3)/3;
+
+    if ($MEDIA<=4){
+        echo "REPROVADO!";
+    }
+    else{
+        echo "APROVADO!";
+    }
+?>
